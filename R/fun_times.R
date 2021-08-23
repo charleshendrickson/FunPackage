685182895077b94cdd462a48787ea3f34b9cfcd7
charleshendrickson <c_d_h@bren.ucsb.edu>
2021-08-22 20:45:53 -0700

    Initial commit

---
+++ R/fun_times.R
@@ -0,0 +1,15 @@
+
+#' Function that provides your age
+#'
+#' @param your_age A parameter for your current age
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fun_times <- function(your_age) {
+  age <- print(paste("I am", your_age, "years old!"))
+  return(age)
+}
+
+fun_times(12)
